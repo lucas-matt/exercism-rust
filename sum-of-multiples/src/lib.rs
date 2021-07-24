2ddab9c2403e9c354bb9a377cb8a91cb1eea88b2
Matthew Lucas <lucasmatthew@hotmail.com>
2021-07-24 15:02:44 +0100

    sum of multiples

---
+++ sum-of-multiples/src/lib.rs
@@ -0,0 +1,7 @@
+pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
+    (1 .. limit).filter(|num| {
+        factors.iter()
+            .filter(|&&factor| factor != 0)
+            .any(|factor| num % factor == 0)
+    }).sum()
+}
