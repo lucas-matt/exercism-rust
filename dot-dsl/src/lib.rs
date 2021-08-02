56b9b28d32fa59f56b630eaf28d2455e22528d5f
Matthew Lucas <lucasmatthew@hotmail.com>
2021-08-02 16:28:22 +0100

    Finish dot dsl

--- dot-dsl/src/lib.rs
+++ dot-dsl/src/lib.rs
@@ -1,48 +1,106 @@
 pub mod graph {
 
     use graph_items::node::Node;
+    use graph_items::edge::Edge;
+    use std::collections::HashMap;
 
     pub struct Graph {
-        pub nodes: Vec<graph_items::node::Node>,
-        pub edges: Vec<graph_items::edge::Edge>,
-        pub attrs: Vec<(String, String)>,
+        pub nodes: Vec<Node>,
+        pub edges: Vec<Edge>,
+        pub attrs: HashMap<String, String>,
     }
 
     impl Graph {
+
         pub fn new() -> Self {
             Graph {
                 nodes: Vec::new(),
                 edges: Vec::new(),
-                attrs: Vec::new(),
+                attrs: HashMap::new(),
             }
         }
 
         pub fn with_nodes(mut self, nodes:&Vec<Node>) -> Self {
-            self.nodes.extend(nodes);
+            self.nodes.extend(nodes.iter().cloned());
             self
         }
+
+        pub fn with_edges(mut self, edges:&Vec<Edge>) -> Self {
+            self.edges.extend(edges.iter().cloned());
+            self
+        }
+
+        pub fn with_attrs(mut self, attrs:&[(&str, &str)]) -> Self {
+            self.attrs.extend(attrs.iter()
+                .map(|(x, y)| (x.to_string(), y.to_string())));
+            self
+        }
+
+        pub fn get_node(self, name:&str) -> Option<Node> {
+            let name = name.to_string();
+            self.nodes.iter().find(|node| node.name == name).cloned()
+        }
+
     }
 
     pub mod graph_items {
 
         pub mod edge {
 
-            pub struct Edge;
+            #[derive(Clone, PartialEq, Debug)]
+            pub struct Edge {
+                source: String,
+                target: String,
+                attrs: Vec<(String, String)>
+            }
+
+            impl Edge {
+
+                pub fn new(source:&str, target:&str) -> Self {
+                    Edge {
+                        source: source.to_string(),
+                        target: target.to_string(),
+                        attrs: Vec::new()
+                    }
+                }
+
+                pub fn with_attrs(mut self, attrs:&[(&str, &str)]) -> Self {
+                    self.attrs.extend(attrs.iter()
+                        .map(|(x, y)| (x.to_string(), y.to_string())));
+                    self
+                }
+
+            }
 
         }
 
         pub mod node {
 
+            #[derive(Clone, PartialEq, Debug)]
             pub struct Node {
-                name: String
+                pub name: String,
+                attrs: Vec<(String, String)>
             }
 
             impl Node {
 
                 pub fn new(name: &str) -> Self {
                     Node {
-                        name: name.to_string()
+                        name: name.to_string(),
+                        attrs: vec!()
                     }
+                }
+
+                pub fn with_attrs(mut self, attrs:&[(&str, &str)]) -> Self {
+                    self.attrs.extend(attrs.iter()
+                        .map(|(x, y)| (x.to_string(), y.to_string())));
+                    self
+                }
+
+                pub fn get_attr(&self, attr:&str) -> Option<&str> {
+                    let attr = attr.to_string();
+                    self.attrs.iter().find(|(k, _)| k.clone() == attr)
+                        .map(|(_, v)| &v[..])
                 }
 
             }
