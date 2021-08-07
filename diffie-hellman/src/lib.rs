b3c0ef71321bf8059d4ce5db35ebbedce47a94b3
Matthew Lucas <lucasmatthew@hotmail.com>
2021-08-07 16:44:55 +0100

    Finished diffie

--- diffie-hellman/src/lib.rs
+++ diffie-hellman/src/lib.rs
@@ -1,15 +1,22 @@
 use rand::Rng;
 use num::bigint::{ToBigInt, ToBigUint};
+use num::{BigUint, ToPrimitive};
 
 pub fn private_key(p: u64) -> u64 {
     let mut rng = rand::thread_rng();
     rng.gen_range(2..p)
 }
 
+fn modpow(x: u64, y: u64, z: u64) -> u64 {
+    let args:Vec<BigUint> = vec!(x, y, z).into_iter().map(|n| n.to_biguint().unwrap()).collect();
+    let (x, y, z) = (args[0].clone(), args[1].clone(), args[2].clone());
+    y.modpow(&z, &x).to_u64().unwrap()
+}
+
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
-    g.to_biguint().pow(a.to_biguint()) % p.to_biguint()
+    modpow(p, g, a)
 }
 
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
-    b_pub.pow(a as u32) % p
+    modpow(p, b_pub, a)
 }
