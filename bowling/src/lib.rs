03faa887e92c51d96c7fcb7b3579eebac2e484f3
Matt Lucas <matt.lucas@nagra.com>
2021-08-20 19:59:20 +0100

    Tiny bit more

--- bowling/src/lib.rs
+++ bowling/src/lib.rs
@@ -7,54 +7,93 @@ pub enum Error {
 }
 
 pub struct BowlingGame {
-    rolls: Vec<Box<dyn Frame>>
+    frames: Vec<Box<dyn Frame>>
 }
 
 trait Frame {
 
     fn done(&self) -> bool;
 
+    fn roll(&mut self, score:u16);
+
+    fn score(&self) -> u16;
+
 }
 
-struct Standard(Option<i32>, Option<i32>);
+struct Standard(Option<u16>, Option<u16>);
 
 impl Frame for Standard {
 
     fn done(&self) -> bool {
-        todo!()
+        match self {
+            Standard(Some(_), Some(_)) => true,
+            _ => false
+        }
+    }
+
+    fn roll(&mut self, score: u16) {
+        match self {
+            Standard(None, None) => self.0 = Some(score),
+            Standard(Some(_), None) => self.1 = Some(score),
+            _ => panic!("Too many rolls")
+        }
+    }
+
+    fn score(&self) -> u16 {
+        [self.0, self.1].iter().filter(|o| o.is_some()).map()
     }
 
 }
 
-struct Final(Option<i32>, Option<i32>, Option<i32>);
+struct Final(Option<u16>, Option<u16>, Option<u16>);
 
 impl Frame for Final {
 
     fn done(&self) -> bool {
-        todo!()
+        match self {
+            Final(Some(10), Some(10), None) => false,
+            Final(_, None, _) => false,
+            _ => true
+        }
     }
 
+    fn roll(&mut self, score: u16) {
+        match self {
+            Final(None, None, None) => self.0 = Some(score),
+            Final(Some(_), None, None) => self.1 = Some(score),
+            Final(Some(10), Some(10), None) => self.2 = Some(score),
+            _ => panic!("Too many rolls")
+        }
+    }
 }
 
 
 impl BowlingGame {
 
     pub fn new() -> Self {
+        let mut frames:Vec<Box<dyn Frame>> = (0..10).map(|_| Box::new(Standard(None, None)) as Box<dyn Frame>).collect();
+        frames.push(Box::new(Final(None, None, None)));
         BowlingGame {
-            rolls: vec!(Box::new(Standard(None, None)))
+            frames
         }
     }
 
     fn done(&self) -> bool {
-        (&self.rolls).into_iter().all(|frame| frame.done())
+        (&self.frames).into_iter().all(|frame| frame.done())
+    }
+
+    fn next(&mut self) -> Option<&mut Box<dyn Frame>> {
+        (&mut self.frames).into_iter().filter(|frame| !frame.done()).next()
     }
 
     pub fn roll(&mut self, pins: u16) -> Result<(), Error> {
-        if self.done() {
-            return Err(GameComplete)
+        match self.next() {
+            None => Err(GameComplete),
+            Some(frame) => {
+                frame.roll(pins);
+                Ok(())
+            }
         }
-
-        Ok(())
     }
 
     pub fn score(&self) -> Option<u16> {
