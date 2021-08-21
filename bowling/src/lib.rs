ad9c70e7e05431fce4dd86f509240930392ab755
Matt Lucas <matt.lucas@nagra.com>
2021-08-21 12:53:52 +0100

    Tweak

--- bowling/src/lib.rs
+++ bowling/src/lib.rs
@@ -1,4 +1,4 @@
-use crate::Error::GameComplete;
+use crate::Error::{GameComplete, NotEnoughPinsLeft};
 
 #[derive(Debug, PartialEq)]
 pub enum Error {
@@ -14,13 +14,20 @@ trait Frame {
 
     fn done(&self) -> bool;
 
-    fn roll(&mut self, score:u16);
+    fn roll(&mut self, score:u16) -> Result<(), Error>;
 
     fn score(&self) -> u16;
 
 }
 
 struct Standard(Option<u16>, Option<u16>);
+
+fn validate_roll(score: u16, total: u16, max: u16) -> Result<(), Error> {
+    if score > 10 || (score + total) > max {
+        return Err(NotEnoughPinsLeft)
+    }
+    Ok(())
+}
 
 impl Frame for Standard {
 
@@ -31,16 +38,26 @@ impl Frame for Standard {
         }
     }
 
-    fn roll(&mut self, score: u16) {
+    fn roll(&mut self, score: u16) -> Result<(), Error> {
+        validate_roll(score, self.score(), 10)?;
         match self {
-            Standard(None, None) => self.0 = Some(score),
-            Standard(Some(_), None) => self.1 = Some(score),
-            _ => panic!("Too many rolls")
+            Standard(None, None) => {
+                self.0 = Some(score);
+                if score == 10 {
+                    self.1 = Some(0)
+                }
+                Ok(())
+            },
+            Standard(Some(_), None) => { self.1 = Some(score); Ok(()) },
+            _ => Err(NotEnoughPinsLeft)
         }
     }
 
     fn score(&self) -> u16 {
-        [self.0, self.1].iter().filter(|o| o.is_some()).map()
+        [self.0, self.1].iter()
+            .filter(|o| o.is_some())
+            .map(|opt| opt.unwrap())
+            .sum()
     }
 
 }
@@ -51,27 +68,34 @@ impl Frame for Final {
 
     fn done(&self) -> bool {
         match self {
-            Final(Some(10), Some(10), None) => false,
-            Final(_, None, _) => false,
+            Final(Some(10), Some(10), None) | Final(_, None, None)  => false,
             _ => true
         }
     }
 
-    fn roll(&mut self, score: u16) {
+    fn roll(&mut self, score: u16) -> Result<(), Error> {
         match self {
-            Final(None, None, None) => self.0 = Some(score),
-            Final(Some(_), None, None) => self.1 = Some(score),
-            Final(Some(10), Some(10), None) => self.2 = Some(score),
-            _ => panic!("Too many rolls")
+            Final(None, None, None) => { self.0 = Some(score); Ok(()) },
+            Final(Some(_), None, None) => { self.1 = Some(score); Ok(()) },
+            Final(Some(_), Some(_), None) => { self.2 = Some(score); Ok(()) },
+            _ => Err(NotEnoughPinsLeft)
         }
     }
+
+    fn score(&self) -> u16 {
+        [self.0, self.1, self.2].iter()
+            .filter(|o| o.is_some())
+            .map(|opt| opt.unwrap())
+            .sum()
+    }
+
 }
 
 
 impl BowlingGame {
 
     pub fn new() -> Self {
-        let mut frames:Vec<Box<dyn Frame>> = (0..10).map(|_| Box::new(Standard(None, None)) as Box<dyn Frame>).collect();
+        let mut frames:Vec<Box<dyn Frame>> = (0..9).map(|_| Box::new(Standard(None, None)) as Box<dyn Frame>).collect();
         frames.push(Box::new(Final(None, None, None)));
         BowlingGame {
             frames
@@ -89,10 +113,7 @@ impl BowlingGame {
     pub fn roll(&mut self, pins: u16) -> Result<(), Error> {
         match self.next() {
             None => Err(GameComplete),
-            Some(frame) => {
-                frame.roll(pins);
-                Ok(())
-            }
+            Some(frame) => frame.roll(pins)
         }
     }
 
@@ -100,7 +121,7 @@ impl BowlingGame {
         if !self.done() {
             return None;
         }
-        Some(0)
+        Some((&self.frames).into_iter().map(|frame| frame.score()).sum())
     }
 
 }
