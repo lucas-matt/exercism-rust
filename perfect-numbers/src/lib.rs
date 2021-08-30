2c695475e09233e779c29470403c1a2b33712bb3
Matthew Lucas <lucasmatthew@hotmail.com>
2021-08-30 07:12:53 +0100

    Perfect numbers

--- perfect-numbers/src/lib.rs
+++ perfect-numbers/src/lib.rs
@@ -1,3 +1,6 @@
+use std::cmp::Ordering;
+use crate::Classification::{Perfect, Abundant, Deficient};
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum Classification {
     Abundant,
@@ -6,5 +9,16 @@ pub enum Classification {
 }
 
 pub fn classify(num: u64) -> Option<Classification> {
-    unimplemented!("classify {}", num);
+    if num < 1 {
+        return None;
+    }
+    let factors = (1..num)
+        .filter(|f| num != *f)
+        .filter(|f| num % f == 0);
+    let aliquot:u64 = factors.sum();
+    match num.cmp(&aliquot) {
+        Ordering::Equal => Some(Perfect),
+        Ordering::Greater => Some(Deficient),
+        Ordering::Less => Some(Abundant)
+    }
 }
