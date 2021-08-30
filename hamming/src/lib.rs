3880f808abaf2dd7fad73f6b8fd7578c3c888fac
Matthew Lucas <lucasmatthew@hotmail.com>
2021-08-30 07:01:59 +0100

    Hamming

--- hamming/src/lib.rs
+++ hamming/src/lib.rs
@@ -1,5 +1,11 @@
 /// Return the Hamming distance between the strings,
 /// or None if the lengths are mismatched.
 pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
-    unimplemented!("What is the Hamming Distance between {} and {}", s1, s2);
+    if s1.len() != s2.len() {
+        return None;
+    }
+    let distance = s1.chars().zip(s2.chars())
+        .filter(|(x, y)| x != y)
+        .count();
+    Some(distance)
 }
