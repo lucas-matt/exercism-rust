9e1293141bb78740705f6c68368cd295f58c5fc5
Matt Lucas <matt.lucas@nagra.com>
2021-09-02 18:34:43 +0100

    Palindrome products

--- palindrome-products/src/lib.rs
+++ palindrome-products/src/lib.rs
@@ -1,26 +1,41 @@
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Copy, Clone, PartialOrd)]
 pub struct Palindrome {
-    // implement your palindrome type here
+    a:u64, b:u64
 }
 
 impl Palindrome {
     pub fn new(a: u64, b: u64) -> Palindrome {
-        unimplemented!("create a palindrome with factors ({}, {})", a, b)
+        Palindrome {
+            a, b
+        }
     }
 
     pub fn value(&self) -> u64 {
-        unimplemented!("return the value of this palindrome")
+        self.a * self.b
     }
 
     pub fn insert(&mut self, a: u64, b: u64) {
-        unimplemented!("insert new factors ({}, {}) into this palindrome", a, b)
+        self.a = a;
+        self.b = b;
     }
 }
 
 pub fn palindrome_products(min: u64, max: u64) -> Option<(Palindrome, Palindrome)> {
-    unimplemented!(
-        "Find the min and max palindromic numbers which are products of numbers in the inclusive range ({}..{})",
-        min,
-        max
-    )
+    let palindomes:Vec<Palindrome> = (min..=max).flat_map(|x| {
+        (min..=max)
+            .filter(move |y| y <= &x)
+            .map(move |y| Palindrome::new(y, x))
+    })
+        .filter(|p| is_palindrome(&p.value()))
+        .collect();
+    let smallest = palindomes.iter().min();
+    let largest = palindomes.iter().max();
+    match (smallest, largest) {
+        (Some(&smallest), Some(&largest)) => Some((smallest, largest)),
+        _ => None
+    }
+}
+
+fn is_palindrome(p: &u64) -> bool {
+    p.to_string().chars().collect::<String>() == p.to_string().chars().rev().collect::<String>()
 }
