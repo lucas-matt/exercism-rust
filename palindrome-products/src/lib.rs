588305c3f26b9e013b2ee68be64aa5f2557a5a45
Matt Lucas <matt.lucas@nagra.com>
2021-09-03 18:22:10 +0100

    palindrome-products.iml

--- palindrome-products/src/lib.rs
+++ palindrome-products/src/lib.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, PartialEq, Eq, Copy, Clone, PartialOrd)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Palindrome {
     a:u64, b:u64
 }
@@ -21,21 +21,33 @@ impl Palindrome {
 }
 
 pub fn palindrome_products(min: u64, max: u64) -> Option<(Palindrome, Palindrome)> {
-    let palindomes:Vec<Palindrome> = (min..=max).flat_map(|x| {
-        (min..=max)
-            .filter(move |y| y <= &x)
-            .map(move |y| Palindrome::new(y, x))
-    })
-        .filter(|p| is_palindrome(&p.value()))
+    let max_len = max.to_string().len();
+    let palindromes:Vec<u64> = palindromes(max_len * max_len)
+        .iter()
+        .map(|d| d.parse::<u64>().unwrap())
+        .filter(|&d| min*min <= d && d <= max*max)
         .collect();
-    let smallest = palindomes.iter().min();
-    let largest = palindomes.iter().max();
-    match (smallest, largest) {
-        (Some(&smallest), Some(&largest)) => Some((smallest, largest)),
-        _ => None
-    }
+    None
 }
 
-fn is_palindrome(p: &u64) -> bool {
-    p.to_string().chars().collect::<String>() == p.to_string().chars().rev().collect::<String>()
+const digits:&[char;10] = &['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+pub fn palindromes(size: usize) -> Vec<String> {
+    if size == 1 {
+        let mut single:Vec<String> = digits.to_vec().iter().map(|d| d.to_string()).collect();
+        let double:Vec<String> = single.iter().map(|d| format!("{}{}", d, d)).collect();
+        single.extend(double);
+        return single;
+    }
+    let previous = palindromes(size - 1);
+    let mut palindromes = previous.clone();
+    for palindrome in previous.into_iter() {
+        for digit in digits {
+            palindromes.push(format!("{}{}{}", digit, palindrome, digit))
+        }
+    }
+    palindromes
+        .into_iter()
+        .filter(|p| !p.starts_with('0'))
+        .collect()
 }
