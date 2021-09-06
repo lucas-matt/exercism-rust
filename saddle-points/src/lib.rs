fed38b8991f7aa3e3083a9832316d9d94c930437
Matt Lucas <matt.lucas@nagra.com>
2021-09-06 16:56:15 +0100

    Saddle points

--- saddle-points/src/lib.rs
+++ saddle-points/src/lib.rs
@@ -1,6 +1,14 @@
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    unimplemented!(
-        "find the saddle points of the following matrix: {:?}",
-        input
-    )
+    let mut saddle_points:Vec<(usize, usize)> = Vec::new();
+    for r in 0..input.len() {
+        let row = &input[r];
+        for c in 0..row.len() {
+            let col: Vec<u64> = input.iter().map(|row| row[c]).collect();
+            let point = row[c];
+            if row.into_iter().all(|&r| point >= r) && col.into_iter().all(|c| point <= c) {
+                saddle_points.push((r, c))
+            }
+        }
+    }
+    saddle_points
 }
