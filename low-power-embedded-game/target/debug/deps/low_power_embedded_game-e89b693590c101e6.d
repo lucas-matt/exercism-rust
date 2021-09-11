055c8e3fa7b9c1ff783a62b9cbe776b8c97cb061
Matt Lucas <matt.lucas@nagra.com>
2021-09-11 07:50:37 +0100

    Low power

---
+++ low-power-embedded-game/target/debug/deps/low_power_embedded_game-e89b693590c101e6.d
@@ -0,0 +1,7 @@
+\\wsl$\Ubuntu-20.04\home\lucas\exercism\rust\low-power-embedded-game\target\debug\deps\low_power_embedded_game-e89b693590c101e6.rmeta: src\lib.rs
+
+\\wsl$\Ubuntu-20.04\home\lucas\exercism\rust\low-power-embedded-game\target\debug\deps\liblow_power_embedded_game-e89b693590c101e6.rlib: src\lib.rs
+
+\\wsl$\Ubuntu-20.04\home\lucas\exercism\rust\low-power-embedded-game\target\debug\deps\low_power_embedded_game-e89b693590c101e6.d: src\lib.rs
+
+src\lib.rs:
---
+++ low-power-embedded-game/target/debug/deps/low_power_embedded_game-f52d2fdcea42de88.d
@@ -0,0 +1,5 @@
+\\wsl$\Ubuntu-20.04\home\lucas\exercism\rust\low-power-embedded-game\target\debug\deps\low_power_embedded_game-f52d2fdcea42de88.exe: tests\low-power-embedded-game.rs
+
+\\wsl$\Ubuntu-20.04\home\lucas\exercism\rust\low-power-embedded-game\target\debug\deps\low_power_embedded_game-f52d2fdcea42de88.d: tests\low-power-embedded-game.rs
+
+tests\low-power-embedded-game.rs:
