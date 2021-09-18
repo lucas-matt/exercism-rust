3e2663b9f62ee0f6af33cc8ef0b8bf283c4d3456
Matthew Lucas <lucasmatthew@hotmail.com>
2021-09-18 10:21:36 +0100

    rpm

--- rpn-calculator/src/lib.rs
+++ rpn-calculator/src/lib.rs
@@ -1,5 +1,4 @@
 use crate::CalculatorInput::{Value, Add, Subtract, Multiply, Divide};
-use std::collections::HashMap;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum CalculatorInput {
@@ -14,24 +13,37 @@ pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
     let mut sum = inputs.to_vec();
     while sum.len() > 1 {
         let op = sum.iter().enumerate().find(|(_, op)| *&[Add, Subtract, Multiply, Divide].contains(op));
-        match op {
-            None => break,
-            Some((i, op)) => {
-                if let &[Value(x), Value(y), action] = sum.drain(i-2..=i).as_slice() {
-                    let value = match action {
-                        Add => Value(x+y),
-                        Subtract => Value(x-y),
-                        Multiply => Value(x*y),
-                        Divide => Value(x/y),
-                        _ => panic!("Unsupported action")
-                    };
-                    sum.insert(i-2, value);
-                };
-            }
+        sum = match op {
+            Some((i, _)) if i >= 2 => {
+                let head = &sum[..i-2];
+                let tail = &sum[i+1..];
+                head.iter()
+                    .chain(eval(&sum[i-2], &sum[i-1], &sum[i]).iter())
+                    .chain(tail.iter())
+                    .map(|c| *c)
+                    .collect()
+            },
+            _ => break
         }
     }
     match sum.as_slice() {
         &[Value(v)] => Some(v),
         _ => None
     }
+}
+
+fn eval(x:&CalculatorInput, y:&CalculatorInput, func:&CalculatorInput) -> Vec<CalculatorInput> {
+    let result = match (x, y, func){
+        (Value(x), Value(y), func) => {
+            match func {
+                Add => x + y,
+                Subtract => x - y,
+                Multiply => x * y,
+                Divide => x / y,
+                _ => panic!("Unsupported")
+            }
+        },
+        _ => panic!("Unsupported")
+    };
+    vec!(Value(result))
 }
