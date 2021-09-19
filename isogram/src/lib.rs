b30f4b37955f87332a4a29e7dcbccca58c2429eb
Matthew Lucas <lucasmatthew@hotmail.com>
2021-09-19 18:22:32 +0100

    isogram

--- isogram/src/lib.rs
+++ isogram/src/lib.rs
@@ -1,3 +1,10 @@
 pub fn check(candidate: &str) -> bool {
-    unimplemented!("Is {} an isogram?", candidate);
+    let mut chars: Vec<char> = candidate.chars()
+        .filter(|c| c.is_alphabetic())
+        .map(|c| c.to_ascii_lowercase())
+        .collect();
+    let size = chars.len();
+    chars.sort();
+    chars.dedup();
+    size == chars.len()
 }
